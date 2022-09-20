136d3af47c1b1ef3797279125ec9d4896e611097
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2022-09-20 21:14:32 +0300

    дополнил метод greetings в соответствии с требованиями

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 """Генератор приветствий."""
-import pprint
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Args:
+        name: Имя пользователя
+
+    Returns:
+        str: Текст приветствия
+    """
+    cor_name = str(name.title())
+    return "Привет, %s" % cor_name  # noqa: WPS323, Q000
